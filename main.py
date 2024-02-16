b72f755099dd38586db039ea3a83bc27cb800a08
tolkien <tolkien@donatello.tail3237d.ts.net>
2024-02-16 22:30:03 +0000

    Commented and improved readability. Added print_to_file_from_csv()

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+#################################################################################
+# This script was developed by Julian Soto with aid from Jimmy Carrero Acosta   #
+# with the purpose of elaborating a program capable of determining if a given   #
+# automata is a finite state automata. This is meant as a project for the COMP  #
+# 5045 class from the University of Puerto Rico - Mayagüez.                     #
+#                                                                               #
+# Initial version completed Friday, February 16, 2024                           #
+#                                                                               #
+#################################################################################
+
 # Python program to find strongly connected components in a given
 # directed graph using Tarjan's algorithm (single DFS)
-#Complexity : O(V+E) taraj exclusively, we have not calculated the entire app
+
+# Complexity : O(V+E) taraj exclusively, we have not calculated the entire app
+
 import sys
 import pandas as pd
 import numpy as np
-from collections import defaultdict
+
+debug = 0
+
 # This class represents an directed graph
 # using adjacency list representation
+
+#################################################################################
+# This tarjan class applies the tarjan algorithm and returns a list of connected#
+# components when given an adjacency list. No idea how it works.                #
+#################################################################################
 class TarjanSCC:
 
     def __init__(self, graphAux:dict):
@@ -72,8 +91,14 @@ class TarjanSCC:
                     break
             self.scc_list.append(scc)
                        
-"""dataframe (representacion del csv) into adjacency list """
-def unweightedDirected (dataFrame):
+#################################################################################
+# Returns an adjacency list (dictionary) when given a pandas dataframe (CSV     #
+# file).                                                                        #
+#                                                                               #
+# Input:    Data frame from pandas (CSV file)                                   #
+# Output:   An adjacency list (dictionary) representing the graph               #
+#################################################################################
+def unweighted_directed(dataFrame):
 
     graph = {}
     
@@ -97,12 +122,18 @@ def unweightedDirected (dataFrame):
     
     return graph
 
-"""auxiliar para convertir lista de adyacencia a matriz de incidencia"""
-def adjacency_list_to_incidence_matrix(adj_list):
+#################################################################################
+# Returns an incidence matrix when given an adjacency list.                     #
+#                                                                               #
+# Input:    An adjacency list (dictionary {source: target})                     #
+# Output:   An incidence matrix  (list of lists)                                #
+#################################################################################
+def adjacency_list_to_incidence_matrix(adj_list:dict) -> list[list[int]]:
     # Extract vertices and edges
     vertices = list(adj_list.keys())
     edges = set()
 
+    # Add all edges into a set so only one instance of each set
     for neighbors in adj_list.values():
         edges.update(neighbors)
 
@@ -118,17 +149,27 @@ def adjacency_list_to_incidence_matrix(adj_list):
             edge_index = edge_index_map[neighbor]
             incidence_matrix[vertex_index][edge_index] = 1
     """
-    nailuj otos ereh saw 
+    nailuj otos saw ereh
     """
     return incidence_matrix
 
-"""auxiliar para imprimir"""
+#################################################################################
+# Prints a matrix in a pretty way.                                              #
+#                                                                               #
+# Input:    A matrix (2D list)                                                  #
+# Output:   Prints matrix to console                                            #
+#################################################################################
 def print_aligned_matrix(matrix):
     for row in matrix:
         print(" ".join(["{:2}".format(item) for item in row]))
 
-"""verifica si toda la diagonal es diferente a 0"""
-def check_diagonal(matrix):
+#################################################################################
+# Returns True if the given matrix all non-zero value in its diagonal.          #
+#                                                                               #
+# Input:    A matrix                                                            #
+# Output:   True if it has a non-zero value in the diagonal, False otherwise    #
+#################################################################################
+def has_all_non_zeros_diagonal(matrix):
     n = len(matrix)
     
     # Check if the matrix is square
@@ -148,134 +189,175 @@ def check_diagonal(matrix):
     
     return True
 
-""" verifica si almenos un valor es diferente a 0"""
-def has_nonzero_diagonal(matrix):
-    """
-    Checks if at least one value in the diagonal of a symmetric matrix is nonzero.
-    
-    Args:
-        matrix (list of lists): The symmetric matrix.
-        
-    Returns:
-        bool: True if at least one value in the diagonal is nonzero, False otherwise.
-    """
-    n = len(matrix)
-    for i in range(n):
+#################################################################################
+# Returns true if the given matrix has at least one non-zero value in its       #
+# diagonal.                                                                     #
+#                                                                               #
+# Input:    A matrix                                                            #
+# Output:   True if it has a non-zero value in the diagonal, False otherwise    #
+#################################################################################
+def has_nonzero_diagonal(matrix:list) -> bool:
+    length = len(matrix)
+    for i in range(length):
         if matrix[i][i] != 0:
             return True
     return False
 
-"""encuentra todas las potencias de una matriz que tengan al menos un non-zero en su diagonal y las devuelve un arreglo"""
-def find_nonzero_powers(mat):
+#################################################################################
+# Given a square matrix "M" of dimensions (n x n), returns all powers of the    # 
+# "g" such that M^g has at least one non-zero element in its diagonal.          #
+#                                                                               #
+# Input:    A square matrix                                                     #
+# Output:   A list of all powers g such that that M^g has at least one non-zero #
+#           element in its diagonal                                             #
+#################################################################################
+def find_nonzero_powers(original_matrix:list) -> list:
 
-    aux_matrix = mat
-    returner = []
+    elevated_matrix = original_matrix
+    non_zero_powers = []
     
-    if len(mat) == 1:
-        returner.append(1)
-        return returner
-   
+    # If the matrix is (1 x 1), return the single element
+    # THIS MIGHT CAUSE A LOGIC ERROR IF THE ELEMENT IS 0!!!!
+    if len(original_matrix) == 1:
+        non_zero_powers.append(1)
+        return non_zero_powers
+    
 
-    for x in range (len(mat)):
+    # Repeat from 1 to n, where n is the dimension of the matrix
+    for element in range(len(original_matrix)):
+        # Check if this power has a non-zero element in the diagonal
+        if has_nonzero_diagonal(elevated_matrix):
+            # Add it to the non_zero_powers list    
+            non_zero_powers.append(element + 1)
         
-        if has_nonzero_diagonal(aux_matrix):
-           returner.append(x+1)
-          
-        aux_matrix = np.matmul(aux_matrix,mat)
+        # Calculate the next power of the matrix and assign it to elevated_matrix
+        elevated_matrix = np.matmul(elevated_matrix,original_matrix)
        
     
-    return returner
+    return non_zero_powers
 
-"""devuelve el gcd de todos los elementos en un arreglo de enteros"""
-def gcd_array(array):
-#initialize variable b as first element of A
-    if len(array)==1:
-        return array[0]
-    x = np.gcd.reduce(array)
-    return x
+#################################################################################
+# Returns the Greates Common Divisor (GCD) of all of the elements in a given    #
+# array.                                                                        #
+#                                                                               #
+# Input: An array of ints                                                       #
+# Output: An int representing the GCD                                           #
+#################################################################################
+def gcd_array(array: list[int]) -> int:
+    if (debug == 1):
+        gcd = np.gcd.reduce(array)
+        print("GCD: ", gcd)
+        return gcd
+    return np.gcd.reduce(array)
 
-"""hace sub listas de adyacencia de lista de componentes (un arreglo a la vez) fuertemente conectado"""
-def make_sub_adjList(connectedComponents: list, graph: dict):
+
+#################################################################################
+# Makes an adjacency list from an array of nodes and a dictionary that contains #
+# the relations to be established.                                              #
+#                                                                               #
+# Input: List of nodes, Dictionary containing their relations (or edges)        #
+# Output: A dictionary in the form of with source nodes as keys, and a list of  #
+#         target nodes as a value.                                              #
+#################################################################################
+def make_sub_adj_list(connectedComponents: list, graph: dict) -> dict:
     
-    auxConnected = sorted(connectedComponents)
+    aux_connected = sorted(connectedComponents)
 
-    auxAdjacency = {}
+    aux_adjacency = {}
     
-    for element in auxConnected:
+    for element in aux_connected:
         
-        auxAdjacency.update({element:[]})
+        aux_adjacency.update({element:[]})
     
-    for element in auxAdjacency:
+    for element in aux_adjacency:
         
-        for item in auxConnected:
+        for item in aux_connected:
             
             if item in graph[element]:
                 
-                auxAdjacency[element].append(item)
+                aux_adjacency[element].append(item)
     
-    return auxAdjacency
+    return aux_adjacency
 
-"""consigue los loop numbers de una lista que contenga todos los scc (listas) de un grafo"""
-def all_loop_numbers_scc(sccList:list, motherGraph:dict):
+#################################################################################
+# Given a list of strongly connected components and their connections,          #
+# return a list that contains the loop numbers of those components.             #
+#                                                                               #
+# Input: 2D list of strongly connected components                               #
+# Output: List of all loop numbers in no particular order                       #
+#################################################################################
+def all_loop_numbers_scc(scc_list:list, mother_graph:dict) -> list:
     
-    returner = []
-    tempDict = {}
+    loop_numbers = []
+    temp_dict = {}
 
-    for scc in sccList:
+    for scc in scc_list:
             
-            # tempDict = make_sub_adjList(scc ,motherGraph)
-            # incidence = adjacency_list_to_incidence_matrix(tempDict)
-            # non_zeros = find_nonzero_powers(incidence)
-            # print(non_zeros)
-            returner.append(gcd_array(find_nonzero_powers(adjacency_list_to_incidence_matrix(tempDict))))
-            
+            temp_dict = make_sub_adj_list(scc, mother_graph)
+            if (debug == 1):
+                # Segment each variable to facilitate debugging
+                incidence_matrix = adjacency_list_to_incidence_matrix(temp_dict)
+                powers_with_non_zero_values = find_nonzero_powers(incidence_matrix)
+                gcd = gcd_array(powers_with_non_zero_values)
+                print("tmp_dictionary: ",               temp_dict)
+                print("incidence_matrix: ",             incidence_matrix)
+                print("powers_with_non_zero_values: ",  powers_with_non_zero_values)
+                print("gcd: ",                          gcd) 
 
-    return returner
+                loop_numbers.append(gcd)
+            else :
+                loop_numbers.append(gcd_array(find_nonzero_powers(adjacency_list_to_incidence_matrix(temp_dict))))
+                # loop_numbers.append(gcd_array(find_nonzero_powers(adjacency_list_to_incidence_matrix(temp_dict))))
+                return loop_numbers
+            
+def find_loop_number(scc:list, graph:dict)-> int:
+    # temp_dict = make_sub_adj_list(scc, graph)
+    loop_number = gcd_array(find_nonzero_powers(adjacency_list_to_incidence_matrix(make_sub_adj_list(scc, graph))))
+    return loop_number
+
+def print_to_file_from_csv(path_to_csv: str):
+    # create a pandas data frame from the given CSV file
+    data_frame = pd.read_csv(path_to_csv)
+
+    # Create a graph (dictionary) from the data frame
+    mother_graph = unweighted_directed(data_frame)
+
+    # Apply tarjan to the graph
+    tarjan = TarjanSCC(mother_graph)
+    tarjan.tarjan()
+    # Also apply the modification that allows for self loops
+    tarjan.filter(mother_graph)
+
+    # Create a new file called "output.txt", replacing any preexisisting file with the same name
+    with open('output.txt','w') as data:
+
+        # Show the complete graph as a dictionary
+        data.write("Your graph looks like this: \n\n")
+        data.write(str(mother_graph))
+        data.write("\n\n")
+
+        # Get the strongly connected components
+        for scc in tarjan.scc_list:
+            # Print the component
+            data.write("Component: \n")
+            data.write(str(scc))
+            # Print the component's graph
+            data.write("\nComponent's graph: \n")
+            data.write(str(make_sub_adj_list(scc, mother_graph)))
+            # Print the component's loop number
+            data.write("\nLoop number: \n")
+            data.write(str(find_loop_number(scc,mother_graph)))
+            # Add 2 new lines 
+            data.write("\n\n")
 
 """todas las funciones necesarias aplicadas al csv"""
 def main():
 
-# Create a graph given in the above diagram
-    fileName = sys.argv[1] ##commandline file name input
-    dF = pd.read_csv(fileName) ##auxiliary function from pandas to convert to csv into manipulatable data frame
+    # Create a graph given in the above diagram
 
-    graph = unweightedDirected(dF)
-    tarjan = TarjanSCC(graph)
-    tarjan.tarjan()
-    tarjan.filter(graph)
-
-    with open('output.txt','w') as data:
-
-        data.write("Your graph looks like this: ")
-        data.write(str(graph))
-        data.write("\n")
-
-        data.write("Strongly Connected Components: \n")
-        counter = 1
-        for scc in tarjan.scc_list:
-            
-            data.write(str(sorted(scc)))
-            counter +=1
-            if counter == len(tarjan.scc_list):
-                data.write(" , ")
-        data.write("\n")
-        
-        
-        
-        for scc in tarjan.scc_list:
-            auxDict = make_sub_adjList(scc, graph)
-            data.write(str(auxDict))
-            data.write(" gcd of power of matrices -> ")
-            data.write(str(gcd_array(find_nonzero_powers(adjacency_list_to_incidence_matrix(auxDict)))))
-            data.write("\n")
-        #aux.append(gcd_array(find_nonzero_powers(adjacency_list_to_incidence_matrix(make_sub_adjList(scc, graph)))))
-
-    #print(aux)
+    # Save the path to the csv file given as a command line argument
+    file_path = sys.argv[1]
+    print_to_file_from_csv(file_path)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-# This code is contributed by Neelam Yadav
